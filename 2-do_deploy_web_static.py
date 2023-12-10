ad81550a8057794dd3a392fe07f8dc6a4b0f37ab
Mcrymbo <croco2023@gmail.com>
2023-12-10 12:08:57 +0300

    deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,18 +24,15 @@ def do_deploy(archive_path):
         wname = shlex.split(wname)
         wname = wname[0]
 
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
+        put(archive_path, "/tmp/{}".format(name))
+        run("mkdir -p /data/web_static/releases/{}".format(wname))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(name, wname))
+        run("rm -rf /tmp/{}".format(name))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(wname, wname))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(wname))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
+        run("ln -s /data/web_static/releases/{} /data/web_static/current".format(wname))
         print("New version deployed!")
         return True
-    except:
+    except Exception:
         return False
