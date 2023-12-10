982897cdd095c1f24647a019c543705b50bcf83b
Mcrymbo <croco2023@gmail.com>
2023-12-10 12:27:42 +0300

    full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
-""" Function that deploys """
-from datetime import datetime
+"""
+Fabric script that distributes and archive
+"""
 from fabric.api import *
-import os
+from datetime import datetime
 import shlex
+import os
 
 
 env.hosts = ['3.83.238.99', '54.236.16.109']
-env.user = "ubuntu"
-
-
-def deploy():
-    """ DEPLOYS """
-    try:
-        archive_path = do_pack()
-    except:
-        return False
-
-    return do_deploy(archive_path)
-
+env.user = 'ubuntu'
 
 def do_pack():
+    """ a function that generates .tgz from content
+    of a folder"""
+
     try:
         if not os.path.exists("versions"):
-            local('mkdir versions')
-        t = datetime.now()
-        f = "%Y%m%d%H%M%S"
-        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
-        local('tar -cvzf {} web_static'.format(archive_path))
-        return archive_path
+            local("mkdir versions")
+
+        time = datetime.now()
+        fm = "%Y%m%d%H%M%S"
+        path = 'versions/web_static_{}.tgz'.format(time.strftime(fm))
+        result = local('tar -cvzf {} web_static'.format(path))
+        if result.succeeded:
+            size = os.stat(path).st_size
+            print("web_static packed: {} -> {}Bytes".format(path, size))
+            return path
+        else:
+            return None
     except:
         return None
 
@@ -46,18 +46,24 @@ def do_deploy(archive_path):
         wname = shlex.split(wname)
         wname = wname[0]
 
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
+        put(archive_path, "/tmp/{}".format(name))
+        run("mkdir -p /data/web_static/releases/{}".format(wname))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(name, wname))
+        run("rm -rf /tmp/{}".format(name))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(wname, wname))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(wname))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
+        run("ln -s /data/web_static/releases/{} /data/web_static/current".format(wname))
         print("New version deployed!")
         return True
-    except:
+    except Exception:
         return False
+
+def deploy():
+    """
+    creates and distributes an archive to web server
+    """
+    arc = do_pack()
+    if arc is None:
+        return False
+    return do_deploy(arc)
