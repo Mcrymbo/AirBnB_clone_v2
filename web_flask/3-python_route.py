635a954d74f6093c4d72fe17c5f695c8a214e378
Mcrymbo <croco2023@gmail.com>
2023-12-27 21:47:20 +0300

    web flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -11,15 +11,18 @@ def hello_hbnb():
     """a function that defines / route """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ defines /hbnb route"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """ allows a text to be passed in the url """
     return 'C is {}'.format(text)
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -28,6 +31,7 @@ def python_is_cool(text='is cool'):
     if text is not 'is cool':
         text = text.replace('_', ' ')
     return 'Python {}'.format(text)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -11,15 +11,18 @@ def hello_hbnb():
     """a function that defines / route """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ defines /hbnb route"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """ allows a text to be passed in the url """
     return 'C is {}'.format(text)
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -29,15 +32,18 @@ def python_is_cool(text='is cool'):
         text = text.replace('_', ' ')
     return 'Python {}'.format(text)
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def is_number(n):
     """ display number passed to url """
     return '%d is a number' % n
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """ display html template with number """
     return render_template('5-number.html', number=n)
+
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
@@ -48,7 +54,6 @@ def odd_or_even(n):
     else:
         context['result'] = 'odd'
     return render_template('6-number_odd_or_even.html', context=context)
-
 
 
 if __name__ == '__main__':
