22af936fcd65858cd79b30e142cb9d8f3fb1368c
Mcrymbo <croco2023@gmail.com>
2023-12-28 09:13:54 +0300

    states added

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+fetches data from storage engine and serves them using flask
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    states = storage.all(State)
+    context = {value.id: value.name for value in states.values()}
+    return render_template('7-states_list.html', Table="States", items=context)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
